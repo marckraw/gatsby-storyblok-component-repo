ac5194cbaeafbcc84cb522c502cb64fc7dba31e8
Marcin Krawczyk <marckraw@icloud.com>
2020-02-06 22:08:00 +0100

    lerna init

---
+++ @storyblok-components/advanced-image/advanced.image.sb.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: "advanced-image",
+    display_name: "Advanced image",
+    is_root: false,
+    is_nestable: true,
+    schema: {
+        title: {
+            type: "text"
+        }
+    }
+};
